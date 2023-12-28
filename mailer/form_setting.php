1359f4a4c713fb2ec20dd2436ceb71d72ee615d3
hardcorekid03 <parasapremiumko88@gmail.com>
2023-12-28 11:06:26 +0800

    first

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Anthony F. Tolentino - CV'"; //letter subject
+	$to = 'strawhatlaw003@gmail.com'; // Enter Your E-mail
+	$from = 'evb1601e@gmail.com'; // Admin e-mail
+	$fromName = 'Anthony F. Tolentino - CV'; // Your company name
+	$charset = 'UTF-8';
+?>
